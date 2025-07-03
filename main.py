ea57e94c523b344b49ae60179e7e21b657987192
eo85 <eric.oneill85@gmail.com>
2025-07-02 20:33:26 -0500

    black rectangle

---
+++ main.py
@@ -0,0 +1,20 @@
+#def main():
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill((0,0,0))
+
+if __name__ == "__main__":
+    main()
